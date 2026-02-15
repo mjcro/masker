9f0b47ba20bb259194d507faf7bb66be91faf622
gotterdemarung <gotterdemarung@gmail.com>
2026-02-15 21:14:00 +0200

    :sparkles: XML support

--- src/test/java/io/github/mjcro/masker/xml/XmlStringStaxMaskerTest.java
+++ src/test/java/io/github/mjcro/masker/xml/XmlStringStaxMaskerTest.java
@@ -4,6 +4,7 @@ import io.github.mjcro.masker.rules.DefaultObjectFieldsRulebook;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.xml.stream.XMLOutputFactory;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -13,6 +14,7 @@ public class XmlStringStaxMaskerTest {
         String given = Files.readString(Path.of(getClass().getClassLoader().getResource("xml-given.xml").toURI()));
         String expected = Files.readString(Path.of(getClass().getClassLoader().getResource("xml-expected.xml").toURI()));
 
+        System.out.println("Factory Class: " + XMLOutputFactory.newInstance().getClass().getName());
         Assertions.assertEquals(
                 expected,
                 XmlStringStaxMasker.usingRulebook(new DefaultObjectFieldsRulebook()).applyMasking(given)
